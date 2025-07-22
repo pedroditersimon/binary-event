7ad78297c91fa41660668206226a4f718d56b625
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2025-07-22 20:00:21 -0300

    feat: added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { Route, BrowserRouter, Routes, Navigate } from "react-router";
+import { LoginPage } from "src/features/auth/pages/LoginPage";
+import { QRScanPreparePage } from "src/features/qr-scan/pages/QRScanPreparePage";
+import { QRScanSuccessPage } from "src/features/qr-scan/pages/QRScanSuccessPage";
 
 export function App() {
     return (
-        <p className="bg-red-200 text-xl p-2 rounded-full">Hello, World!</p>
+        <BrowserRouter>
+            <Routes>
+
+                <Route path="/" element={<Navigate to="login" />}></Route>
+
+                <Route path="login" Component={LoginPage}></Route>
+
+                <Route path="scan" >
+                    <Route path="" Component={QRScanPreparePage}></Route>
+                    <Route path="success" Component={QRScanSuccessPage}></Route>
+                </Route>
+
+            </Routes>
+        </BrowserRouter>
     );
 }
