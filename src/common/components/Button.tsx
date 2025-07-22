8c826e7343780ceb7c0233978b1436ac9c72197c
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2025-07-22 20:00:30 -0300

    feat: enhance Button component with navigation and click handling

--- src/common/components/Button.tsx
+++ src/common/components/Button.tsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router";
 import { cn } from "src/common/utils/cn";
 
 interface Props {
     className?: string;
     label: string;
     icon?: string;
+
+    navigateTo?: string;
+    onClick?: () => void;
 }
 
-export function Button({ className, label, icon }: Props) {
+export function Button({ className, label, icon, navigateTo, onClick }: Props) {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        if (navigateTo) navigate(navigateTo);
+        if (onClick) onClick();
+    }
 
     return (
         <button
@@ -14,6 +24,7 @@ export function Button({ className, label, icon }: Props) {
                 "w-full bg-primary hover:bg-primary-hover hover:cursor-pointer text-white font-semibold p-2 rounded-2xl",
                 className
             )}
+            onClick={handleClick}
         >
             <div className="flex flex-row items-center">
                 <span className="w-full">{label}</span>
