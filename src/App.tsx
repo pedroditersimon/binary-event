4ceda6be6a09b3b15f3ef4f68566cc619a75fb3c
pedroditersimon <114523969+pedroditersimon@users.noreply.github.com>
2025-07-23 19:25:40 -0300

    feat: toaster and some routes updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import { Route, BrowserRouter, Routes, Navigate } from "react-router";
-import { LoginPage } from "src/features/auth/pages/LoginPage";
+import { Toaster } from 'sonner';
+import { LoginPage } from "src/features/auth/login/LoginPage";
+import { HomePage } from "src/features/home/HomePage";
+import { NotFoundPage } from "src/features/not-found/NotFoundPage";
 import { QRScanPreparePage } from "src/features/qr-scan/pages/QRScanPreparePage";
 import { QRScanReaderPage } from "src/features/qr-scan/pages/QRScanReaderPage";
 import { QRScanSuccessPage } from "src/features/qr-scan/pages/QRScanSuccessPage";
 
+
 export function App() {
     return (
-        <BrowserRouter>
-            <Routes>
+        <>
+            <Toaster position="top-right" duration={8000} />
+            <BrowserRouter>
+                <Routes >
 
-                <Route path="/" element={<Navigate to="login" />}></Route>
+                    <Route path="*" Component={NotFoundPage} />
 
-                <Route path="login" Component={LoginPage}></Route>
+                    <Route path="/" element={<Navigate to="login" />} />
 
-                <Route path="scan" >
-                    <Route path="" Component={QRScanPreparePage}></Route>
-                    <Route path="success" Component={QRScanSuccessPage}></Route>
-                    <Route path="on" Component={QRScanReaderPage}></Route>
-                </Route>
+                    <Route path="home" Component={HomePage} />
 
-            </Routes>
-        </BrowserRouter>
+                    <Route path="login" Component={LoginPage} />
+
+                    <Route path="scan" >
+                        <Route path="" Component={QRScanPreparePage} />
+                        <Route path="success" Component={QRScanSuccessPage} />
+                        <Route path="on" Component={QRScanReaderPage} />
+                    </Route>
+
+                </Routes>
+            </BrowserRouter>
+        </>
+
     );
 }
